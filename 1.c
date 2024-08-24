847c09b906a354f3002314c1d13f9649c631795c
Nimra Lateef <ismailzm01@gmail.com>
2024-08-25 01:44:23 +0500

    first commit

--- 1.c
+++ 1.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
-int mian (){
-    printf("hello");
-    return 0;
+#include <stdio.h>
+// main function -
+// where the execution of program begins
+int main()
+{
+// prints hello world
+printf("Hello World");
+return 0;
 }
 
 
